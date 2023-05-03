10fa518a51b0db25c19a7a022484757d9439621a
Kirill Khomicevich <khomicevichkirill@icloud.com>
2023-05-03 21:29:44 +0300

    Initial Commit

---
+++ second_simple_Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  second_simple_Firebase
+//
+//  Created by Kirill Khomytsevych on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
